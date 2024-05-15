33ae9a1ec1885132781c8655baaa341c64780305
Engin Sühan Bilgiç <enginsuhan.bilgic@borsaistanbul.com>
2024-05-15 15:05:18 +0300

    Server initialization commit

---
+++ src/main/java/com/knowitall/myFmp/MyFmpApplication.java
@@ -0,0 +1,13 @@
+package com.knowitall.myFmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowitall/myFmp/MyFmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowitall.myFmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
